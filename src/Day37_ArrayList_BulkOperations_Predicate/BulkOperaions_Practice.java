ad9b8d2301a852bde5e89574ce37a2b60c6a28a8
StanislavRak <rakstas51@Gmail.com>
2022-04-24 13:48:38 +0200

    Lambda Expression

--- src/Day37_ArrayList_BulkOperations_Predicate/BulkOperaions_Practice.java
+++ src/Day37_ArrayList_BulkOperations_Predicate/BulkOperaions_Practice.java
@@ -1,6 +1,6 @@
 package Day37_ArrayList_BulkOperations_Predicate;
 
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,7 +21,52 @@ Integer in[]={10,20,30,40,50,65,14,22,65,985};
        System.out.println(max);
 
         System.out.println("==========================");
-//remove method
+//remove method (delete all the matching elements)
+        System.out.println("remove method");
+        ArrayList<Integer>nums = new ArrayList<>();
+        nums.addAll( Arrays.asList(1,1,2,2,3,3,4,5,5));
+        nums.removeAll(Arrays.asList(1,3,5));
+        System.out.println(nums);
+        System.out.println();
+
+//retainAll (delete all the NOT matching elements)
+        System.out.println("retainAll");
+        ArrayList<Integer>nums1 = new ArrayList<>();
+        nums1.addAll( Arrays.asList(1,1,2,2,3,3,4,5,5));
+        nums1.removeAll(Arrays.asList(4,2));
+        System.out.println(nums1);
+        System.out.println();
+
+        System.out.println("=======================");
+        System.out.println("remove name");
+        System.out.println();
+        System.out.println("Task 1");
+        ArrayList<String> names =new ArrayList<>();
+        names.addAll(Arrays.asList("Nadiya", "Oleksiy", "Lucky","Alinka", "Emilichka"));
+        names.removeAll(Arrays.asList("Lucky"));
+        System.out.println(names);
+
+
+        System.out.println("=============");
+        /*write a program that can remove the elements thta are not unique from the ArrayList
+
+			NOTE:
+				DO NOT USE:
+						for loop
+						while loop
+						for each loop
+						do-while
+
+
+						list: {1,1,2,2,3,3,4,5,6,7}
+
+						output: {4,5,6,7}*/
+        System.out.println("Task 2");
+        ArrayList<Integer>task2=new ArrayList<>();
+        task2.addAll(Arrays.asList(1,1,2,2,3,3,4,5,6,7));
+
+
+
 
 
 
---
+++ src/Day37_ArrayList_BulkOperations_Predicate/Lambla_Expression.java
@@ -0,0 +1,51 @@
+package Day37_ArrayList_BulkOperations_Predicate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class Lambla_Expression {
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,1,1,2,2,3,4,4,5,6,7,8,9));
+
+       // Predicate<Integer>numLess5=p->p<5;
+        list.removeIf(p->p<5);
+        System.out.println(list);
+
+
+        ArrayList<Integer>list1 = new ArrayList<>();
+        list1.addAll(Arrays.asList(1,1,1,2,2,3,4,4,5,6,7,8,9));
+        Predicate<Integer>oddNum=p->p %2!=0;  //OddNumbers
+        list1.removeIf(oddNum);
+        System.out.println(list1);
+        ArrayList<Integer>list2 = new ArrayList<>();
+        list2.addAll(Arrays.asList(1,1,1,2,2,3,4,4,5,6,7,8,9));
+        Predicate<Integer>evenNum=p->p %2==0;  //evenNumbers
+        list2.removeIf(evenNum);
+        System.out.println(list2);
+        System.out.println("=======================");
+
+        ArrayList<String>names=new ArrayList<>();
+        names.addAll(Arrays.asList("Marina", "Margarita", "Marusiya", "Moldova", "Moroz", "Alina"));
+        names.removeIf(  each->each.startsWith("M") &&each.endsWith("a") );
+        System.out.println(names);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
