1a8978471284ea75ce5e5e2e977b0ddaa79d4fb5
StanislavRak <rakstas51@Gmail.com>
2022-04-24 12:05:04 +0200

    remove method

--- src/Day37_ArrayList_BulkOperations_Predicate/BulkOperaions_Practice.java
+++ src/Day37_ArrayList_BulkOperations_Predicate/BulkOperaions_Practice.java
@@ -3,6 +3,7 @@ package Day37_ArrayList_BulkOperations_Predicate;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class BulkOperaions_Practice {
     public static void main(String[] args) {
@@ -10,6 +11,21 @@ public class BulkOperaions_Practice {
         list.addAll(Arrays.asList("A","B","C","D","E","F"));
         boolean verify = list.containsAll(Arrays.asList("B", "F"));
         System.out.println(verify);
+
+ArrayList<Integer>list2=new ArrayList<>();
+Integer in[]={10,20,30,40,50,65,14,22,65,985};
+        list2.addAll(Arrays.asList(in));
+       int max= Collections.max(list2);
+
+       System.out.println(list2);
+       System.out.println(max);
+
+        System.out.println("==========================");
+//remove method
+
+
+
+
 
 
 
