8e9acc02c9cc4677456e2f6e95c92f0ba2ca2516
StanislavRak <rakstas51@Gmail.com>
2022-06-13 13:26:52 +0200

    Static VS instance

--- src/Day56_OOP_AbstractionContinueInterface/InterfacePractice.java
+++ src/Day56_OOP_AbstractionContinueInterface/InterfacePractice.java
@@ -8,7 +8,20 @@ public interface InterfacePractice {
 
     static void method1(){
         System.out.println(a);
+    }
 
+    public static void main(String[] args) {
 
     }
+    default void method2(){}
+    public abstract void method3();
+    //public void method4(){};  instance method
+    void method5(); //abstract method
+
+
+
+
+
+
+
 }
